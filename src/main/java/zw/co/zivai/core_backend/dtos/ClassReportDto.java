77bd443c050a778fd050522f01af059eea0044f5
Mcdonald <mcdonald.mpofu@students.uz.ac.zw>
2026-02-09 21:21:11 +0200

    bulk apis

---
+++ src/main/java/zw/co/zivai/core_backend/dtos/ClassReportDto.java
@@ -0,0 +1,22 @@
+package zw.co.zivai.core_backend.dtos;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class ClassReportDto {
+    String subjectId;
+    String subjectName;
+    String classId;
+    String className;
+    double classAveragePercent;
+    String predictedGrade;
+    int studentCount;
+    int assessmentCount;
+    Map<String, Long> gradeDistribution;
+    List<CurriculumTopicForecastDto> masteryGaps;
+}
